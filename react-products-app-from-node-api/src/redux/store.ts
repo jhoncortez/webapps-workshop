4736734fbe4d468898101342701703e44cd509df
jhoncortez <jhoncortezbastidas@gmail.com>
2025-06-20 18:07:12 -0500

    react-products-shop-app-from-node-api-v5.1.JWT-auth-frontend-redux-query-persist-cart

--- react-products-app-from-node-api/src/redux/store.ts
+++ react-products-app-from-node-api/src/redux/store.ts
@@ -1,29 +1,16 @@
 // store.ts
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage"
 // import productsReducer from './features/products/productsSlice';
 import cartReducer from './features/cart/cartSlice';
 
-const persistConfig = {
-  key: 'cart', // key for the auth state
-  storage, // storage mechanism (local storage or session storage)
-  // whitelist: [
-  //   // 'cart',
-  //   'auth'
-  // ], // which reducers to persist
-};
-
-const persistedReducer = persistReducer(persistConfig, cartReducer);
-
-
-// Configure the Redux store
-const store: any = configureStore({ 
-  reducer: persistedReducer
+const store = configureStore({
+  reducer: {
+    // products: productsReducer,
+    cart: cartReducer,
+  },
 });
 
 export default store;
 
-export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
